4bd67571781aee1562f8ea7238a99dcc0f8b4e6f
Ilhom Aliyev <aliyevilhom99@gmail.com>
2023-06-08 11:30:31 +0500

    some changes

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { Reorder } from 'framer-motion';
+import { ChangeEvent, useState } from 'react';
 import { FilterValuesType } from '../../App';
 import AddItemForm from '../AddItemForm/AddItemForm';
 import EditableSpan from '../EditableSpan/EditableSpan';
@@ -27,6 +27,8 @@ type PropsType = {
 };
 
 const ToDoList = (props: PropsType) => {
+  const [tasks, setTasks] = useState(props.tasks);
+
   const allFilter = () => props.changeFilter('all', props.id);
   const activeFilter = () => props.changeFilter('active', props.id);
   const completedFilter = () => props.changeFilter('completed', props.id);
@@ -52,51 +54,65 @@ const ToDoList = (props: PropsType) => {
         onChangeHandler={changeToDoListNameHandler}
       />
       <AddItemForm inputPlaceholder="Task name..." addItem={addTask} />
-      <ul className="ToDoList__list">
-        <TransitionGroup>
-          {props.tasks.length !== 0 &&
-            props.tasks.map((t) => {
-              const removeTaskHandler = () => {
-                props.removeTask(t.id, props.id);
-              };
 
-              const isDoneChangeHandler = (
-                event: ChangeEvent<HTMLInputElement>
-              ) => {
-                props.changeTaskStatus(
-                  t.id,
-                  event.currentTarget.checked,
-                  props.id
-                );
-              };
-
-              const taskTitleChange = (newTitle: string) => {
-                props.changeTaskName(props.id, t.id, newTitle);
-              };
-
-              return (
-                <CSSTransition key={t.id} timeout={350} classNames={'taskItem'}>
-                  <li className={t.isDone ? 'taskItem' : 'taskItem _isDone'}>
-                    <input
-                      type="checkbox"
-                      checked={t.isDone}
-                      onChange={isDoneChangeHandler}
-                    />
-                    <EditableSpan
-                      elemClassName="taskTitle"
-                      title={t.title}
-                      onChangeHandler={taskTitleChange}
-                    />
-                    <button id="delete-btn" onClick={removeTaskHandler}>
-                      X
-                    </button>
-                  </li>
-                </CSSTransition>
+      <Reorder.Group
+        className="ToDoList__list"
+        axis="y"
+        values={tasks}
+        onReorder={setTasks}
+      >
+        {/* <ul className="ToDoList__list"> */}
+        {tasks.length !== 0 &&
+          tasks.map((t) => {
+            const removeTaskHandler = () => {
+              props.removeTask(t.id, props.id);
+            };
+            const isDoneChangeHandler = (
+              event: ChangeEvent<HTMLInputElement>
+            ) => {
+              props.changeTaskStatus(
+                t.id,
+                event.currentTarget.checked,
+                props.id
               );
-            })}
-        </TransitionGroup>
-        {props.tasks.length === 0 && <h4>There are no tasks!</h4>}
-      </ul>
+            };
+            const taskTitleChange = (newTitle: string) => {
+              props.changeTaskName(props.id, t.id, newTitle);
+            };
+
+            return (
+              <Reorder.Item
+                key={t.id}
+                className={t.isDone ? 'taskItem _isDone' : 'taskItem'}
+                value={t}
+                whileDrag={{
+                  //! PAUSE 11:30 08.06.2023
+                  transform: 'scale(1.1)',
+                  // transform: 'rotate(-5deg)',
+                  // transformOrigin: '0% 0%',
+                }}
+              >
+                {/* <li> */}
+                <input
+                  type="checkbox"
+                  checked={t.isDone}
+                  onChange={isDoneChangeHandler}
+                />
+                <EditableSpan
+                  elemClassName="taskTitle"
+                  title={t.title}
+                  onChangeHandler={taskTitleChange}
+                />
+                <button id="delete-btn" onClick={removeTaskHandler}>
+                  X
+                </button>
+                {/* </li> */}
+              </Reorder.Item>
+            );
+          })}
+        {tasks.length === 0 && <h4>There are no tasks!</h4>}
+        {/* </ul> */}
+      </Reorder.Group>
       <div className="buttons-wrapper">
         <button
           className={props.filter === 'all' ? '_active' : ''}
